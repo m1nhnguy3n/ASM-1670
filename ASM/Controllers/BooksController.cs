c43de9d9feba88f5ef0295342bee7d8462cad31c
minhnn20021 <minhnn2001@github.com>
2022-05-08 00:17:54 +0700

    Add project files.

---
+++ ASM/Controllers/BooksController.cs
@@ -0,0 +1,188 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASM.Data;
+using Demo6.Models;
+using Microsoft.AspNetCore.Identity;
+using ASM.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ASM.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly UserContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public BooksController(UserContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            AppUser thisUser = await _userManager.GetUserAsync(HttpContext.User);
+            Store thisStore = await _context.Stores.FirstOrDefaultAsync(s => s.UId == thisUser.Id);
+            var books = _context.Books
+                .Where(b => b.StoreId == thisStore.Id)
+                .Include(b => b.Store);
+            return View(await books.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Store)
+                .FirstOrDefaultAsync(m => m.Isbn == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Seller")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Isbn,Title,Pages,Author,Category,Price,Decs,Imgurl,StoreId")] Book book, IFormFile image)
+        {
+            if (image != null)
+            {
+
+                //Set Key Name
+                string ImageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+
+                //Get url To Save
+                string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", ImageName);
+
+                using (var stream = new FileStream(SavePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                AppUser thisUser = await _userManager.GetUserAsync(HttpContext.User);
+                Store thisStore = await _context.Stores.FirstOrDefaultAsync(s => s.UId == thisUser.Id);
+                book.StoreId = thisStore.Id;
+            }
+            else
+            {
+                return View(book);
+            }
+
+            _context.Add(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id", book.StoreId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Isbn,Title,Pages,Author,Category,Price,Decs,Imgurl,StoreId")] Book book)
+        {
+            if (id != book.Isbn)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Isbn))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id", book.StoreId);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Store)
+                .FirstOrDefaultAsync(m => m.Isbn == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(string id)
+        {
+            return _context.Books.Any(e => e.Isbn == id);
+        }
+    }
+}
---
+++ ASM/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using ASM.Areas.Identity.Data;
+
+
+namespace Demo6.Models
+{
+    public class Cart
+    {
+        public string UId { get; set; }
+        public string BookIsbn { get; set; }
+        public AppUser? User { get; set; }
+        public Book? Book { get; set; }
+    }
+}
